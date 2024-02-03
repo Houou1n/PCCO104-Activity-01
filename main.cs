5bafb67610190e214e2ab1a4f01a1c3e6e12cea5
r1intaro <31803625-r1intaro@users.noreply.replit.com>
2024-02-03 02:44:21 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "BIOT"
+    Console.WriteLine ($"Hello, {firstName}");
+    Console.WriteLine ($"Hello, {firstName}");
+  }
+}
