89ea49ba6463a36d0c91e26ce7525a0d95976f85
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-02-03 03:22:47 +0000

    Greetings

--- main.cs
+++ main.cs
@@ -4,8 +4,9 @@ class Program {
   public static void Main (string[] args) {
     string firstName = "BIOT";
     string lastName = "KA";
+    string greetings = "hahahhaahh";
     int age = 17;
     Console.WriteLine ($"Hello {firstName} {lastName} Age: {age}");
-    Console.WriteLine ($"Hello " + firstName + " " + lastName + " " + age);
+    Console.WriteLine ($"Hello " + firstName + " " + lastName + " " + age + " " + greetings);
   }
 }
