f4e9897a5656f81e5b7bc9a9f544f5fe48507520
r1intaro <31803625-r1intaro@users.noreply.replit.com>
2024-02-03 02:50:05 +0000

    test

--- main.cs
+++ main.cs
@@ -2,8 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    string firstName = "BIOT"
-    Console.WriteLine ($"Hello, {firstName}");
-    Console.WriteLine ($"Hello, {firstName}");
+    string firstName = "BIOT";
+    string lastName = "KA";
+    int age = 17;
+    Console.WriteLine ($"Hello {firstName} {lastName}");
+    Console.WriteLine ($"Hello " + firstName + " " + lastName);
   }
 }
