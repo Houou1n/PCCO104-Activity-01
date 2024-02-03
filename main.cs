1fb96a4dcc5a8b381cba047d8300f4f8411ce90c
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-02-03 03:28:43 +0000

    Sum and Difference

--- main.cs
+++ main.cs
@@ -6,7 +6,11 @@ class Program {
     string lastName = "KA";
     string greetings = "hahahhaahh";
     int age = 17;
+    int sum = 6 + 9;
+    int difference = 8 - 3;
     Console.WriteLine ($"Hello {firstName} {lastName} Age: {age}");
     Console.WriteLine ($"Hello " + firstName + " " + lastName + " " + age + " " + greetings);
+    Console.WriteLine ("Sum: " + sum);
+    Console.WriteLine ("Difference: " + difference);
   }
 }
